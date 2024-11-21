ad9452b34d51323faeb403462086bb76872c0e5e
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2024-11-21 15:54:38 +0500

    add user routes

--- routes/user.Route.js
+++ routes/user.Route.js
@@ -1,12 +1,37 @@
-const {Router}=require("express")
-const { regiterUser, verifiedUser, loginUser } = require("../controllers/user.controller")
-const SignupValidation = require("../validations/signup.validation")
-const { ErrorResult } = require("../validations/result.validation")
+const { Router } = require("express");
+const userController = require("../controllers/user.controller");
+const {
+  SignupValidation,
+  ResetPassword,
+} = require("../validations/signup.validation");
+const { ErrorResult } = require("../validations/result.validation");
+const tokens = require("../middleware/auth.middleware");
 
+const userRoute = Router();
 
-const userRoute=Router()
-
-userRoute.post("/register",SignupValidation,ErrorResult,regiterUser)
-userRoute.get("/verify/:verificationToken",verifiedUser)
-userRoute.post("/login",loginUser)
-module.exports=userRoute
+userRoute.post(
+  "/register",
+  SignupValidation,
+  ErrorResult,
+  userController.regiterUser
+);
+userRoute.get("/verify/:verificationToken", userController.verifiedUser);
+userRoute.post("/login", userController.loginUser);
+userRoute.post("/forget-password", userController.forgetPassword);
+userRoute.post(
+  "/verify-otp",
+  ResetPassword,
+  ErrorResult,
+  userController.verifyOtp
+);
+userRoute.get(
+  "/incoming-refreshed-tokens",
+  tokens.refreshAccessToken,
+  userController.incomingAccessAndRefreshToken
+);
+userRoute.post(
+  "/change-password",
+  tokens.verifyJwt,
+  userController.changeProfilePassword
+);
+module.exports = userRoute;
